b4cc227bc20d79e19d6f6a29257b25b2094234ac
Joaoaalves <48325411+Joaoaalves@users.noreply.github.com>
2023-10-06 18:59:30 -0300

    Removido lazy load.

--- app/page.js
+++ app/page.js
@@ -10,33 +10,13 @@ const FloatingWhatsapp = dynamic(
     ssr: false,
   }
 );
-const About = dynamic(() => import("./components/About"), {
-  ssr: false,
-});
-const Footer = dynamic(() => import("./components/Footer"), {
-  ssr: false,
-});
-const Contato = dynamic(() => import("./components/Contato"), {
-  ssr: false,
-});
-const Servicos = dynamic(() => import("./components/Servicos"), {
-  ssr: false,
-});
 
-const Avaliacoes = dynamic(() => import("./components/Avaliacoes"), {
-  ssr: false,
-});
-
-const Parceiros = dynamic(() => import("./components/Parceiros"), {
-  ssr: false,
-});
-
-// import About from "./components/About";
-// import Footer from "./components/Footer";
-// import Contato from "./components/Contato";
-// import Servicos from "./components/Servicos";
-// import Avaliacoes from "./components/Avaliacoes";
-// import Parceiros from "./components/Parceiros";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Contato from "./components/Contato";
+import Servicos from "./components/Servicos";
+import Avaliacoes from "./components/Avaliacoes";
+import Parceiros from "./components/Parceiros";
 
 export default function Home() {
   return (
